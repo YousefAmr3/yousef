5f70034da80ae65b204d96c29b718eb13b87c6e6
Your Name <you@example.com>
2023-12-21 12:40:16 +0200

    yy

---
+++ main.py
@@ -0,0 +1,27 @@
+# X = float(input("the number is:\n"))
+# if 90 <X<=100:
+#     print("A")
+# elif 80 <X<= 90:
+#     print("B")
+# elif 70 <X<=80:
+#     print("C")
+# elif X < 0:
+#     print("error")
+# else:
+#      print("D")
+# X = [list(input("enter List"))]
+# print(X)
+# print("len(X)")
+# def name(name):
+#     return (f"Hello {name}")
+# print(name(input("Enter Your Name")))
+#
+#
+# num1 = int(input("enter first num:  "))
+# num2 = int(input("enter second num:  "))
+# def pro(num1,num2):
+#     return num1, num2
+# print(num1+num2)
+# pro(num1,num2)
+
+
